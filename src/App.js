f66f0444c5e54efe7c0e257f74e05c5dc1e6776e
KermitTheSapo <rjsf06@gmail.com>
2022-07-08 21:36:25 -0300

    inicio da atividade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// crie um component de class com um state com uma array de nomes e mostre na tela com um h1
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from "react";
+
+class App extends Component {
+  state = {
+    nome: "Robert",
+    idade: 18,
+    comidaFavorita: "Lasanha",
+    musica: ["riptide", "The Nights", "home"]
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comidaFavorita}</h3>
+        <ul>
+          <li>{this.state.musica[0]}</li>
+          <li>{this.state.musica[1]}</li>
+          <li>{this.state.musica[2]}</li>
+        </ul>
+      </div>
+    ) 
+  }
 }
 
 export default App;
